50930fcca49b1e847f0c2217e7f0c2d9cd59a26b
renn15 <steven.darren.wijaya15@gmail.com>
2025-11-30 00:12:32 +0800

    Added 'Coming Soon' message for the 'secret' page

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Secret = () => {
   return (
-    <div>
-        About me
-        <h1>ME!</h1>
+    <div className="bg-gray-700 w-[full] h-screen flex flex-col justify-center items-center">
+        <div className='text-6xl font-bold text-white'>COMING (not so) SOON</div>
+        <div className='text-2xl text-white'>I have a funny idea but it's kinda outside of the scope for this project so um yeah</div>
     </div>
   )
 }
